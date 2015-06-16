5e1b6d1ca9592107d51d35f4603658a1a307d4b7
quintanalexis1989 <quintanalexis1989@gmail.com>
2015-06-16 00:49:10 -0300

    Comando AltaOfertaLaboral
    algunos cambios mas

--- AltaOfertaLaboral.cpp
+++ AltaOfertaLaboral.cpp
@@ -7,6 +7,8 @@
 #include "DataEmpresa.h"
 #include "DataSucursal.h"
 #include "DataSeccion.h"
+#include "Fabrica.h"
+#include "IEmpresaController.h"
 
 using namespace std;
 AltaOfertaLaboral::AltaOfertaLaboral()
@@ -16,16 +18,15 @@ AltaOfertaLaboral::AltaOfertaLaboral()
 
 void AltaOfertaLaboral::ejecutarComando()
 {
-    string rutaEmpresa, nomSucursal;
-    ManejadorEmpresa* mEmpresa = ManejadorEmpresa::getInstance();
-    EmpresaController* cEmpresa = EmpresaController::getInstance();
+    string rutaEmpresa, nomSucursal, nomSeccion;
+    Fabrica* fab = Fabrica::getInstance();
+    IEmpresaController* cEmpresa = fab->getIEmpresaController();
 
     try
     {
         ICollection* dataEmpresas = cEmpresa->ListarEmpresas();
 
         cout << "Lista de Empresas:\n";
-
         IIterator * it = dataEmpresas->getIterator();
         while(it.hasCurrent())
         {
@@ -42,19 +43,8 @@ void AltaOfertaLaboral::ejecutarComando()
 
         cout<< "Seleccione una Empresa indicando el RUT\n";
         cin >> rutEmpresa;
+        cEmpresa->SeleccionarEmpresa(rutaEmpresa);
 
-        IDictionary* empresas = mEmpresa->getEmpresas();
-        String* rut = new String(rutaEmpresa);
-
-        //Este tipo de control de errores se hacea este nivel ?????
-        if(empresas->member(rut))
-        {
-            cEmpresa->SeleccionarEmpresa(rutaEmpresa);
-        }
-        else
-        {
-            throw std::invalid_argument("La Empresa con RUT " + rutaEmpresa + " no existe en el sistema.");
-        }
 
         ICollection* dataSucursales = cEmpresa->ListarSucursales();
         cout << "Lista de Sucursales:\n";
@@ -75,7 +65,56 @@ void AltaOfertaLaboral::ejecutarComando()
 
         cout<< "Seleccione una Sucursal indicando el nombre\n";
         cin >> nomSucursal;
-        Empresa* empresa = cEmpresa->getEmpresa();
+        cEmpresa->SeleccionarSucursal(nomSucursal);
+
+        ICollection* dataSecciones = cEmpresa->ListarSecciones();
+        cout<< "Lista de Secciones:\n";
+        IIterator * it3 = dataSecciones->getIterator();
+        while(it3.hasCurrent())
+        {
+            DataSeccion *dSeccion;
+            if( (dSeccion = dynamic_cast<DataSeccion*> (it3.current())) != NULL )
+            {
+                cout << "Nombre: " + dSeccion->getNombre() + "\n";
+            } else
+            {
+                throw std::invalid_argument("AltaOfertaLaboral -> El objeto no es de la clase DataSeccion.");
+            }
+        }
+        delete it3;
+
+        cout<< "Seleccione una Sección indicando su nombre\n";
+        cin >> nomSeccion;
+        cEmpresa->SeleccionarSeccion(nomSeccion);
+
+        cout<< "Ingrese los datos correspondientes a la nueva Oferta Laboral\n";
+        cout<< "\n";
+        cout<< "Numero de expediente: "
+        cin >> numExpe;
+        cout<< "\nTitulo: ";
+        cin >> titulo;
+        cout<< "\nDescripcion: ";
+        cin >> descripcion;
+        cout<< "\nCantidad de horas semanales: ";
+        cin >> cantHorasSema;
+        cout<< "\nSalario minimo: ";
+        cin >> salMinimo;
+        cout<< "\nSalario maximo: ";
+        cin >> salMaximo;
+        Rango* rangoSalarial = new Rango(salMinimo, salMaximo);
+        cout<< "\nFecha de comienzo(dd mm aaaa): ";
+        cin >> ddCom >> mmCom >> aaaaCom;
+        cout<< "\nFecha de fin(dd mm aaaa): ";
+        cin >> ddFin >> mmFin >> aaaaFin;
+        Date* fechaComienzo = new Date(ddCom, mmCom, aaaaCom);
+        Date* fechaFin = new Date(ddFin, mmFin, aaaaFin);
+        cout<< "\nCantidad de puestos necesarios: ";
+        cin >> cantPuestos;
+        //Solicitar Asignaturas
+
+
+        cEmpresa->altaOfertaLaboral(numExpe, titulo, descripcion, cantHorasSema, rangoSalarial, fechaComienzo, fechaFin, cantPuestos, "AGREGAR ASIGNATURAS")
+
     }
     catch(const std::invalid_argument &e)
     {
