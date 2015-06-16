e76b57673c19507e05e86193735b463e7729dd1a
Fabian Vallejos <vallejosfab@gmail.com>
2015-06-16 01:22:44 -0300

    Comandos y Actualizacion de Makefile
    Agregados los comandos:
    DarDeBajaLlamado y AsignacionDeOfertaAEstudiante
    
    Generado el Makefile para los nuevos .cpp/.h

---
+++ cmdDarDeBajaLlamado.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include "OfertaLaboralController.h"
+#include "ICollection.h"
+#include "DataOfertaLaboral.h"
+
+using namespace std;
+
+// DarDeBajaLlamado
+void cmdDarDeBajaLlamado::ejecutarComando()
+{
+    string numExpediente;
+    OfertaLaboralController *ctrlOL = OfertaLaboralController::getInstance();
+
+    try
+    {
+        // muestro las ofertas
+        ICollection* dataOfertas = ctrlOL->ListarOfertas();
+
+        cout << "Lista de Ofertas Laborales:\n";
+
+        IIterator *it = dataOfertas->getIterator();
+        while (it.hasCurrent())
+        {
+            DataOfertaLaboral *dOferta;
+            if ( (dOferta = dynamic_cast<DataOfertaLaboral*> (it.current())) != NULL )
+            {
+                cout << "NRO. EXPEDIENTE: " + dOferta->getNumExpediente() + ", TITULO:" + dOferta->getTitulo() + "\n";
+            } else
+            {
+                throw std::invalid_argument("cmdDarDeBajaLlamado -> El objeto no es de la clase DataOfertaLaboral.");
+            }
+        }
+        delete it;
+
+        // selecciono el llamado
+        cout<< "Seleccione una Oferta Laboral indicando el Nro. Expediente: \n";
+        cin >> numExpediente;
+
+        ctrlOL->SeleccionarOferta(numExpediente);
+
+        // borro el llamado
+        ctrlOL->DarBajaLlamado();
+    }
+    catch(const std::invalid_argument &e)
+    {
+    	cout << e.what();
+    }
+}
+
+// constructor
+cmdDarDeBajaLlamado::cmdDarDeBajaLlamado()
+{
+
+}
+
+// destructor
+cmdDarDeBajaLlamado::~cmdDarDeBajaLlamado()
+{
+
+}
